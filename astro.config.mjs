67ec6420592afdded362cf5c2562b01d5f5521e7
EzCodeGT <43971574+EzCodeGT@users.noreply.github.com>
2025-09-04 19:51:22 -0600

    Updated template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ezcodegt.github.io',
-    base: '/astrodemo.github.io'
+	site: 'https://ezcodegt.github.io',
+	base: '/astrodemo.github.io',
+	integrations: [mdx(), sitemap()],
 });
