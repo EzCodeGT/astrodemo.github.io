bde0eb1fb38088ac3103e48ebe1a7a40dfa91c78
EzCodeGT <43971574+EzCodeGT@users.noreply.github.com>
2025-09-04 20:21:52 -0600

    Uopdated posts

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,11 +8,22 @@ const blog = defineCollection({
 	schema: ({ image }) =>
 		z.object({
 			title: z.string(),
-			description: z.string(),
+			description: z.string().optional(),
 			// Transform string to Date object
-			pubDate: z.coerce.date(),
+			pubDate: z.coerce.date().optional(),
 			updatedDate: z.coerce.date().optional(),
 			heroImage: image().optional(),
+			// Added new fields
+			draft: z.boolean(),
+			snippet: z.string(),
+			image: z.object({
+				src: z.string(),
+				alt: z.string(),
+			}).optional(),
+			publishDate: z.string().transform((str) => new Date(str)),
+			author: z.string().default("YourCompany"),
+			category: z.string(),
+			tags: z.array(z.string()),
 		}),
 });
 
